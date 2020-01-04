4921ca9e108d790e1d4b8078416eb0fb2b7ab625
David Banks <amoebae@gmail.com>
2020-01-04 06:30:09 +0000

    cleaning up code

--- layer1.h
+++ layer1.h
@@ -4,20 +4,12 @@
 #include "kittysxchoppas.h"
 
 void set_defaults(PlaybackApp * app);
-void
-print_usage (int argc, char **argv);
-
-GList *
-handle_wildcards (const gchar * location);
-
-
-void create_ui (PlaybackApp * app);
-void connect_bus_signals (PlaybackApp * app);
-void
-play_cb (GtkButton * button, PlaybackApp * app);
-void
-pause_cb (GtkButton * button, PlaybackApp * app);
-void
-reset_app (PlaybackApp * app);
+void print_usage(int argc, char **argv);
+GList *handle_wildcards (const gchar *location);
+void create_ui(PlaybackApp *app);
+void connect_bus_signals(PlaybackApp *app);
+void play_cb(GtkButton * button, PlaybackApp *app);
+void pause_cb(GtkButton * button, PlaybackApp *app);
+void reset_app(PlaybackApp *app);
 
 #endif /* LAYER1_H */
